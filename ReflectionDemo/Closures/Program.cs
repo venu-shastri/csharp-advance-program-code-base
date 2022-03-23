a6af06a1e61bafc52340a498249cafbfd3be075e
Venugopal  K (C) <vgk@tritechsamvit.com>
2022-03-23 17:31:49 +0530

    Closures Updated

---
+++ ReflectionDemo/Closures/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Closures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Philips", "Apple", "Siemens", "General Electricals" };
+            Func<string, bool> predicateFunction = StringStartsWithPredicateGenerator("s");
+            FilterStringUsingAnyPredicate(names, predicateFunction);
+
+            predicateFunction = StringStartsWithPredicateGenerator("u");
+            FilterStringUsingAnyPredicate(names, predicateFunction);
+
+            predicateFunction = StringStartsWithPredicateGenerator("v");
+            FilterStringUsingAnyPredicate(names, predicateFunction);
+
+            //FilterStringUsingAnyPredicate(names, (string item)=> { return item.StartsWith("s"); }) ;
+            //FilterStringUsingAnyPredicate(names, (string item) => { return item.StartsWith("v"); });
+            //FilterStringUsingAnyPredicate(names, (string item) => { return item.StartsWith("u"); });
+            //FilterStringUsingAnyPredicate(names, (string item) => { return item.StartsWith("x"); });
+            //FilterStringUsingAnyPredicate(names, (string item) => { return item.StartsWith("z"); });
+
+        }
+
+        static Func<string, bool> StringStartsWithPredicateGenerator(string startsWith)
+        {
+
+            bool FilterMethod(string item)
+            {
+                return item.StartsWith(startsWith);
+            }
+            //Func<string, bool> predicateMethod = (string item) => { return item.StartsWith(startsWith); };
+            return FilterMethod;
+        }
+        //static Func<string, bool> StringLengthPredicateGenerator(int length)
+        //{
+
+        //    //bool FilterMethod(string item)
+        //    //{
+        //    //    return item.StartsWith(startsWith);
+        //    //}
+        //    Func<string, bool> predicateMethod = (string item) => { return item.Length>length };
+        //    return predicateMethod;
+        //}
+
+        public static List<string> FilterStringUsingAnyPredicate(string[] names,Func<string,bool>   predicateFunction)
+        {
+            List<string> result = new List<string>();
+            //iteration
+            for (int i = 0; i < names.Length; i++)
+            {
+                //startswith predicate 
+                if (predicateFunction.Invoke(names[i]))
+                {
+                    result.Add(names[i]);
+                }
+            }
+            return result;
+
+        }
+
+    }
+}
--- ReflectionDemo/Reusability/Program.cs
+++ ReflectionDemo/Reusability/Program.cs
@@ -36,39 +36,48 @@ namespace Reusability
     //    }
     //}
 
-    public class MethodWrapper_Returns_Boolean_Accepts_StringAsArgument
-    {
-        System.Type  target;
-        string methodName;
-        public MethodWrapper_Returns_Boolean_Accepts_StringAsArgument(System.Type target, string methodName)
-        {
-            this.target = target;
-            this.methodName = methodName;
-        }
-        public bool Invoke(string item)
-        {
-            bool result = default(bool);
-            //Reflection
-           System.Reflection.MethodInfo _method= target.GetMethod(this.methodName, 
-                System.Reflection.BindingFlags.Instance | 
-                System.Reflection.BindingFlags.Static |
-                System.Reflection.BindingFlags.Public | 
-                System.Reflection.BindingFlags.NonPublic);
-            if(_method.ReturnType==typeof(bool) && _method.GetParameters().Length ==1 && _method.GetParameters()[0].ParameterType == typeof(string))
-            {
-                result=(bool)_method.Invoke(this.target, new object[] { item });
-               
-            }
-            else
-            {
-                throw new InvalidOperationException("Signature Mismatch");
-            }
-            return result;
 
-        }
-        
 
-    }
+    //Command Pattern
+
+    /* Command - > Encapsulate Request
+     * Command -> delegate calls
+     * */
+    //public class MethodWrapper_Returns_Boolean_Accepts_StringAsArgument
+    //{
+    //    System.Type  target;
+    //    string methodName;
+    //    public MethodWrapper_Returns_Boolean_Accepts_StringAsArgument(System.Type target, string methodName)
+    //    {
+    //        this.target = target;
+    //        this.methodName = methodName;
+    //    }
+    //    public bool Invoke(string item)
+    //    {
+    //        bool result = default(bool);
+    //        //Reflection
+    //       System.Reflection.MethodInfo _method= target.GetMethod(this.methodName, 
+    //            System.Reflection.BindingFlags.Instance | 
+    //            System.Reflection.BindingFlags.Static |
+    //            System.Reflection.BindingFlags.Public | 
+    //            System.Reflection.BindingFlags.NonPublic);
+    //        if(_method.ReturnType==typeof(bool) && _method.GetParameters().Length ==1 && _method.GetParameters()[0].ParameterType == typeof(string))
+    //        {
+    //            result=(bool)_method.Invoke(this.target, new object[] { item });
+
+    //        }
+    //        else
+    //        {
+    //            throw new InvalidOperationException("Signature Mismatch");
+    //        }
+    //        return result;
+
+    //    }
+
+
+    //}
+
+    public delegate bool MethodWrapper_Returns_Boolean_Accepts_StringAsArgument(string item);
     class Program
     {
         static void Main(string[] args)
@@ -78,26 +87,43 @@ namespace Reusability
             // result = FilterStringUsingAnyPredicate(names, new CheckStringStartsWithAny("v"));
             // result = FilterStringUsingAnyPredicate(names, new CheckStringEndsWithAny("a"));
 
-            MethodWrapper_Returns_Boolean_Accepts_StringAsArgument _functionObject = 
-                new MethodWrapper_Returns_Boolean_Accepts_StringAsArgument(typeof(Program), "CheckStringStartsWith_s");
+            //MethodWrapper_Returns_Boolean_Accepts_StringAsArgument _functionObject = 
+            //    new MethodWrapper_Returns_Boolean_Accepts_StringAsArgument(typeof(Program), "CheckStringStartsWith_s");
+            //List<string> result = FilterStringUsingAnyPredicate(names, _functionObject);
+
+            //_functionObject = new MethodWrapper_Returns_Boolean_Accepts_StringAsArgument(typeof(Program),nameof(CheckStringEndWith_s));
+            //result = FilterStringUsingAnyPredicate(names, _functionObject);
+
+            MethodWrapper_Returns_Boolean_Accepts_StringAsArgument _functionObject =
+                Program.CheckStringStartsWithAny("p");
             List<string> result = FilterStringUsingAnyPredicate(names, _functionObject);
 
-            _functionObject = new MethodWrapper_Returns_Boolean_Accepts_StringAsArgument(typeof(Program),nameof(CheckStringEndWith_s));
-            result = FilterStringUsingAnyPredicate(names, _functionObject);
+            _functionObject = Program.CheckStringStartsWithAny("s");
+             result = FilterStringUsingAnyPredicate(names, _functionObject);
             for (int i = 0; i < result.Count; i++)
             {
                 Console.WriteLine(result[i]);
             }
         }
-        public static bool CheckStringStartsWith_s(string item)
+        public static MethodWrapper_Returns_Boolean_Accepts_StringAsArgument CheckStringStartsWithAny(string key)
         {
-            return item.StartsWith("s");
+            //bool CheckStringStartsWithString(string item)
+            //{
+            //    return item.StartsWith(key);
+            //}
+            //Lamda
+           MethodWrapper_Returns_Boolean_Accepts_StringAsArgument functionObject= (string item) => { return item.StartsWith(key); };
+            return functionObject;
         }
-        public static bool CheckStringEndWith_s(string item)
+        public static bool CheckStringStartsWith_p(string item)
+        {
+            return item.StartsWith("p");
+        }
+        public static bool CheckStringEndWith_s(string item,string key)
         {
             return item.EndsWith("s");
         }
-        public static bool CheckStrinLengthGreaterThan_3(string item)
+        public static bool CheckStrinLengthGreaterThan_3(string item,string length)
         {
             return item.Length > 3;
         }
