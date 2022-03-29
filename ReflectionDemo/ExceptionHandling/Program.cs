37703b03f82df9897700de37cc8434ce2df57f43
Venugopal  K (C) <vgk@tritechsamvit.com>
2022-03-29 17:43:20 +0530

    Memory Mgmt

---
+++ ReflectionDemo/ExceptionHandling/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+           System.Threading.Thread _t1 = new System.Threading.Thread(Test);
+            
+            Test();
+        }
+
+        private static void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+        {
+         
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+           
+        }
+
+        static void Test()
+        {
+            Foo();
+        }
+        static void Foo()
+        {
+            try
+            {
+                Fun();
+            }
+            catch(System.Runtime.InteropServices.COMException e)
+            {
+                Console.WriteLine("bla....");
+            }
+            
+           
+
+        }
+        static void Fun()
+        {
+            throw new InvalidOperationException("Fun");
+            
+                
+        }
+
+    }
+}
---
+++ ReflectionDemo/MemoryManagement/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagement
+{
+    public class A
+    {
+        B obj = new B();
+    }
+    public class B
+    {
+
+    }
+    class Program
+    {
+        static A globalRef;
+        static void Main(string[] args)
+        {
+            Allocate();
+            globalRef = null;
+            GC.Collect();//yes
+        }
+        static void Allocate()
+        {
+            A obj = new A();//0
+            Console.WriteLine(GC.GetGeneration(obj));
+            List<A> _collections = new List<A>();
+            _collections.Add(obj);
+            Program.globalRef = obj;
+            GC.Collect();//No
+            Console.WriteLine(GC.GetGeneration(obj)); // 1
+            obj = null;
+            GC.Collect();//No
+            Console.WriteLine(GC.GetGeneration(_collections[0]));//2
+            _collections.Clear();
+            GC.Collect();//No
+            Console.WriteLine(GC.GetGeneration(globalRef)); //2
+
+        }
+    }
+}
